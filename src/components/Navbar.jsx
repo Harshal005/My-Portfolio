2402b2c298590e2a038f2c00d7cdcc82ce9288ba
Harshal005 <harshalpatilpdea@gmail.com>
2026-07-01 11:37:20 +0530

    Added Logo Hamburger Icon and Reach Out Button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import OverlayMenu from './OverlayMenu.jsx'
-import Logo from "../assets/Logo.png"
+import Logo from "../assets/Logo.png";
+import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
 
@@ -11,12 +12,23 @@ const Navbar = () => {
     <>
 
       <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" : "translate-y-full"}`}>
-        <div>
-          <img src={Logo} alt="" />
+        <div className='flex items-center space-x-2'>
+          <img src={Logo} alt="" className='w-8 h-8' />
+          <div className='text-2xl font-bold text-white hidden sm:block'>Harshal</div>
+        </div>
+
+        <div className='block lg:absolute lg:left-1/2 lg:transform lg:translate-x-1/2'>
+          <button onClick={()=> setMenuOpen(true)}
+            className='text-white outline-none text-3xl' aria-label='open Menu'><FiMenu /></button>
+        </div>
+
+        <div className='hidden lg:block '>
+          <a href="#contact" className='bg-gradient-to-r from-pink-500 to-blue-500 text-white px-5 py-2 rounded-full font-medium shadow-lg hover:opacity-90 transition-opacity duration-300'>Reach Out</a>
+
         </div>
 
       </nav>
-      <OverlayMenu/>
+      <OverlayMenu isOpen = {menuOpen} onCLose={() => setMenuOpen(false)}/>
     </>
   )
 }
